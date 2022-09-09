d9dafaeb3c8c1fa6da6f7fc6588b8db31819f666
Stella <stella.me90@gmail.com>
2022-09-09 03:34:02 -0700

    task 0

--- push.c
+++ push.c
@@ -22,13 +22,13 @@ void f_push(stack_t **head, unsigned int counter)
 			fclose(bus.file);
 			free(bus.content);
 			free_stack(*head);
-			exit(EXIT_FAILURE);	}} 
+			exit(EXIT_FAILURE); }}
 	else
 	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE); } 
+		exit(EXIT_FAILURE); }
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
 		addnode(head, n);
